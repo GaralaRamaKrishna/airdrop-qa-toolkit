99a0b1d0a716d84a20f2e6f6d489cb13e273e2fa
Garala Ramakrishna <ramakrishna13808@gmail.com>
2026-06-24 20:40:38 +0530

    Added Bitget Wallet support

--- config/config.py
+++ config/config.py
@@ -1,3 +1,4 @@
+BITGET_WALLET_URL = "https://bitkeep.com"
 BASE_URL = "https://base.org"
 MONAD_URL = "https://testnet.monad.xyz"
 SOMNIA_URL = "https://somnia.network"
@@ -7,3 +8,11 @@ BROWSER = "chrome"
 SCREENSHOT_FOLDER = "screenshots"
 
 REPORT_FOLDER = "reports"
+
+DEFAULT_WALLET = "Bitget Wallet"
+
+SUPPORTED_WALLETS = [
+    "Bitget Wallet",
+    "MetaMask",
+    "Phantom"
+]
